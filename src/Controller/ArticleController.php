77cf38e9408c02a1711c4c58e13e422da64fa2f5
Andrzej Kaczmarek <akczmr@macbook-air-andrzej.home>
2019-04-04 01:23:14 +0200

    dobrze idzie

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Okurde pierwsza stronka');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug
+        ));
+    }
+
+}
